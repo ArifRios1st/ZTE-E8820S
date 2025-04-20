0fbd1facc7e9f9e5da2fd3160f4da98bdb69f17e
Arif Hendika Rios <arifrios1st@gmail.com>
2025-04-20 15:39:22 +0700

    Update Config

---
+++ script/official/diy-part1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git luci-theme-argon https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.conf.default
+echo 'src-git luci-app-argon-config https://github.com/jerrykuku/luci-app-argon-config' >> feeds.conf.default
